1df4cdbda3903b19ea0e929e3f3252ba44b1e4e5
QuixoticVortex <dinoentrails@gmail.com>
2015-03-23 18:00:25 -0700

    Skeleton code and a makefile.

--- uthread.c
+++ uthread.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ucontext.h>
 
 void system_init();
 int uthread_create(void (* func)());
@@ -9,6 +13,26 @@ void uthread_exit();
 
 void system_init() {
 	printf("Test");
+}
+
+int uthread_create(void (* func)()) {
+
+}
+
+int uthread_startIO() {
+
+}
+
+int uthread_endIO() {
+
+}
+
+int uthread_yield() {
+
+}
+
+void uthread_exit() {
+
 }
 
 
